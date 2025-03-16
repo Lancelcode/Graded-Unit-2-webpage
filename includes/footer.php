c2086fc10cda318e9859eaa2544b56733b2806a9
Lancelcode <127974834+Lancelcode@users.noreply.github.com>
2025-03-16 19:39:46 +0000

    unificacion of footers

--- includes/footer.php
+++ includes/footer.php
@@ -1,39 +1,113 @@
-<!-- Footer Scripts -->
-<script
+    <!-- ========== FOOTER SECTION START ========== -->
+
+    <!-- Load scripts once at the bottom -->
+    <script
         src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
         crossorigin="anonymous">
-</script>
+    </script>
 
-<script
+    <script
         src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
         crossorigin="anonymous">
-</script>
+    </script>
 
-<!-- Footer Section -->
-<footer class="footer" class="text-center py-4">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Main Footer Container -->
+    <footer class="container py-5" 
+            style="background-color: #f8f9fa; padding: 10px; text-align: center; margin-top: 50px; border-top: 1px solid #17a2b8; width: 100%;">
+      <div class="row">
+        <div class="col-12 col-md">
+          <!-- Icon example -->
+          <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" 
+               class="bi bi-dice-5" viewBox="0 0 16 16">
+            <path d="M13 1a2 2 0 0 1 2 2v10a2 2 
+                     0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 
+                     0 0 1 2-2zM3 0a3 3 0 0 0-3 3v10a3 
+                     3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 
+                     3 0 0 0-3-3z"/>
+            <path d="M5.5 4a1.5 1.5 0 1 1-3 0 1.5 
+                     1.5 0 0 1 3 0m8 0a1.5 1.5 0 1 
+                     1-3 0 1.5 1.5 0 0 1 3 0m0 
+                     8a1.5 1.5 0 1 1-3 0 1.5 1.5 
+                     0 0 1 3 0m-8 0a1.5 1.5 0 1 
+                     1-3 0 1.5 1.5 0 0 1 3 
+                     0m4-4a1.5 1.5 0 1 1-3 0 
+                     1.5 1.5 0 0 1 3 0"/>
+          </svg>
+          <small class="d-block mb-3 text-muted">&copy; 2024 I-Cinema</small>
+        </div>
 
-    <div style="max-width: 1200px; margin: 0 auto;">
-        <p style="margin: 0; font-size: 14px; color: #6c757d;">&copy; 2024 I-Cinema . All Rights Reserved.</p>
+        <!-- Example columns -->
+        <div class="col-6 col-md">
+          <h5>Features</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Cool stuff</a></li>
+            <li><a class="text-muted" href="#">Stuff for developers</a></li>
+            <li><a class="text-muted" href="#">Another one</a></li>
+          </ul>
+        </div>
+
+        <div class="col-6 col-md">
+          <h5>Resources</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Resource name</a></li>
+            <li><a class="text-muted" href="#">Another resource</a></li>
+            <li><a class="text-muted" href="#">Final resource</a></li>
+          </ul>
+        </div>
+
+        <div class="col-6 col-md">
+          <h5>Resources</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Business</a></li>
+            <li><a class="text-muted" href="#">Education</a></li>
+            <li><a class="text-muted" href="#">Government</a></li>
+          </ul>
+        </div>
+
+        <div class="col-6 col-md">
+          <h5>About</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Locations</a></li>
+            <li><a class="text-muted" href="#">Privacy</a></li>
+            <li><a class="text-muted" href="#">Terms</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- A small divider or just margin here -->
+      <hr style="margin: 2rem 0;">
+
+      <!-- Lower footer with social links, policies, etc. -->
+      <div>
+        <p style="margin: 0; font-size: 14px; color: #6c757d;">
+          &copy; 2024 I-Cinema. All Rights Reserved.
+        </p>
         <nav style="margin: 10px 0;">
-            <a href="/privacy-policy" style="text-decoration: none; color: #007bff; margin-right: 10px;">Privacy Policy</a>
-            <span style="color: #6c757d;">|</span>
-            <a href="/terms-of-service" style="text-decoration: none; color: #007bff; margin-left: 10px;">Terms of Service</a>
+          <a href="/privacy-policy" style="text-decoration: none; color: #007bff; margin-right: 10px;">
+            Privacy Policy
+          </a>
+          <span style="color: #6c757d;">|</span>
+          <a href="/terms-of-service" style="text-decoration: none; color: #007bff; margin-left: 10px;">
+            Terms of Service
+          </a>
         </nav>
         <div style="margin: 10px 0;">
-            <a href="https://www.facebook.com" target="_blank" style="text-decoration: none; margin-right: 10px;">
-                <i class="fa-brands fa-meta"></i> Meta
-            </a>
-            <a href="https://www.twitter.com" target="_blank" style="text-decoration: none; margin-right: 10px;">
-            <i class="fa-brands fa-twitrer"></i> Twitter
-            </a>
-            <a href="https://www.instagram.com" target="_blank" style="text-decoration: none;">
-            <i class="fa-brands fa-instragram"></i> Instagram
-            </a>
+          <!-- Be sure you have Font Awesome loaded somewhere if you use these icons -->
+          <a href="https://www.facebook.com" target="_blank" style="text-decoration: none; margin-right: 10px;">
+            <i class="fa-brands fa-facebook-f"></i> Facebook
+          </a>
+          <a href="https://www.twitter.com" target="_blank" style="text-decoration: none; margin-right: 10px;">
+            <i class="fa-brands fa-twitter"></i> Twitter
+          </a>
+          <a href="https://www.instagram.com" target="_blank" style="text-decoration: none;">
+            <i class="fa-brands fa-instagram"></i> Instagram
+          </a>
         </div>
-    </div>
-</footer>
+      </div>
+    </footer>
+
+    <!-- Close body/html if this is the end of the entire page -->
 </body>
 </html>
