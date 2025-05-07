552eee150aa157c5a7fcf5f1685a7be41ff6ed82
Lancelcode <127974834+Lancelcode@users.noreply.github.com>
2025-05-07 16:40:19 +0100

    db updated

--- database/gradedunit.sql
+++ database/gradedunit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 02, 2025 at 07:47 PM
+-- Generation Time: May 07, 2025 at 05:17 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -41,7 +41,15 @@ CREATE TABLE `community_tips` (
 INSERT INTO `community_tips` (`id`, `user_id`, `message`, `created_at`) VALUES
 (29, 36, 'Hi this is the tip number 1', '2025-05-02 09:25:36'),
 (38, 39, 'yes sir', '2025-05-02 16:45:09'),
-(39, 40, 'hello crif', '2025-05-02 18:22:40');
+(39, 40, 'hello crif', '2025-05-02 18:22:40'),
+(40, 36, 'I want to be the best company ever', '2025-05-07 13:06:53'),
+(41, 36, 'rgthetrwthggre', '2025-05-07 13:37:03'),
+(42, 36, 'trtrhrthhtrthrwrthhrthtrhtr', '2025-05-07 13:37:13'),
+(43, 36, 'trhrhthttehtrureyjerytj', '2025-05-07 13:37:16'),
+(44, 36, 'tyjjytjytyjjyyjtyj', '2025-05-07 13:37:19'),
+(45, 36, 'rgtgrthrttrhtggtt', '2025-05-07 13:37:26'),
+(46, 36, 'ttrtrtr', '2025-05-07 13:37:30'),
+(47, 37, 'hellooooo', '2025-05-07 14:38:56');
 
 -- --------------------------------------------------------
 
@@ -91,11 +99,16 @@ CREATE TABLE `feedback` (
 --
 
 INSERT INTO `feedback` (`id`, `user_id`, `name`, `email`, `message`, `created_at`, `visible_to_public`, `admin_response`, `admin_username`, `admin_response_at`) VALUES
-(10, NULL, 'fumanchu', 'fu@fu', 'testing please', '2025-05-01 21:45:49', 1, 'here you have!', 'admin@admin', '2025-05-02 10:52:49'),
-(11, NULL, 'fumanchu', 'fu@fu', 'what 123', '2025-05-01 21:45:57', 1, 'bruh 321', 'admin@admin', '2025-05-02 10:52:49'),
-(12, 35, 'fumanchu', 'fu@fu', 'yes?', '2025-05-01 21:46:35', 1, 'no!', 'admin@admin', '2025-05-02 10:52:49'),
-(13, 36, 'admin@admin', 'admin@admin', 'sdfghjkl;', '2025-05-02 08:51:59', 1, 'bckhgfv,jhgvb', 'admin@admin', '2025-05-02 10:52:49'),
-(14, 40, 'cris', 'crif@gmail.com', 'I love your website, its brutal', '2025-05-02 17:23:03', 0, NULL, NULL, NULL);
+(10, NULL, 'fumanchu', 'fu@fu', 'testing please', '2025-05-01 21:45:49', 1, 'here you have!', 'admin@admin', '2025-05-07 16:14:18'),
+(11, NULL, 'fumanchu', 'fu@fu', 'what 123', '2025-05-01 21:45:57', 1, 'bruh 321', 'admin@admin', '2025-05-07 16:14:18'),
+(12, 35, 'fumanchu', 'fu@fu', 'yes?', '2025-05-01 21:46:35', 1, 'no!', 'admin@admin', '2025-05-07 16:14:18'),
+(14, 40, 'cris', 'crif@gmail.com', 'I love your website, its brutal', '2025-05-02 17:23:03', 1, 'i love you !!', 'admin@admin', '2025-05-07 16:14:18'),
+(15, 39, 'Joe', 'joe@joe.com', 'this is feedback', '2025-05-02 18:22:20', 1, 'huaaaa', 'admin@admin', '2025-05-07 16:14:18'),
+(16, 39, 'Joe', 'joe@joe.com', 'this is feedback', '2025-05-02 18:31:46', 1, 'let do it', 'admin@admin', '2025-05-07 16:14:18'),
+(18, 36, 'admin@admin', 'admin@admin', 'haaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa test', '2025-05-07 12:32:45', 1, 'sssssss', 'admin@admin', '2025-05-07 16:14:18'),
+(19, 36, 'admin@admin', 'admin@admin', 'ajjajajjajja testing', '2025-05-07 12:32:53', 1, 'ddddd', 'admin@admin', '2025-05-07 16:14:18'),
+(20, 37, 'jamoncin', 'pernil@pernil', 'holaaa', '2025-05-07 13:39:04', 1, 'nope', 'admin@admin', '2025-05-07 16:14:18'),
+(21, 47, 'test2', 'test2@test2', 'testing?', '2025-05-07 15:13:52', 1, 'nope', 'admin@admin', '2025-05-07 16:14:18');
 
 -- --------------------------------------------------------
 
@@ -129,8 +142,18 @@ INSERT INTO `green_calculator_results` (`id`, `user_id`, `total_score`, `green_c
 (101, 36, 75, 7, 1, 2, 'Certificate of Gold 🥇', '🥇', 'Thank you for your contribution! You\'ve unlocked full recognition!', 0, 250.00, '2025-05-02 08:11:10'),
 (102, 36, 75, 7, 1, 2, 'Certificate of Silver 🥈', '🥈', 'Great job! You\'re making a positive environmental impact.', 25, 250.00, '2025-05-02 08:18:40'),
 (103, 37, 65, 5, 3, 2, 'Certificate of Silver 🥈', '🥈', 'Great job! You\'re making a positive environmental impact.', 35, 350.00, '2025-05-02 09:25:59'),
-(105, 39, 60, 4, 4, 2, 'Certificate of Bronze 🥉', '🥉', 'Nice effort! Keep building sustainable habits.', 40, 400.00, '2025-05-02 14:07:17'),
-(106, 40, 80, 7, 2, 1, 'Certificate of Gold 🥇', '🥇', 'Thank you for your contribution! You\'ve unlocked full recognition!', 0, 200.00, '2025-05-02 17:21:40');
+(106, 40, 80, 7, 2, 1, 'Certificate of Gold 🥇', '🥇', 'Thank you for your contribution! You\'ve unlocked full recognition!', 0, 200.00, '2025-05-02 17:21:40'),
+(108, 39, 80, 7, 2, 1, 'Certificate of Gold 🥇', '🥇', 'Thank you for your contribution! You\'ve unlocked full recognition!', 0, 200.00, '2025-05-07 10:51:19'),
+(109, 36, 100, 10, 0, 0, 'Certificate of Gold 🥇', '🥇', 'Outstanding! You\'re leading the way in sustainability.', 0, 0.00, '2025-05-07 12:03:36'),
+(110, 36, 0, 0, 0, 10, 'Certificate of Gold 🥇', '🥇', 'Thank you for your contribution! You\'ve unlocked full recognition!', 0, 1.00, '2025-05-07 12:04:27'),
+(111, 36, 65, 4, 5, 1, 'Certificate of Silver 🥈', '🥈', 'Great job! You\'re making a positive environmental impact.', 35, 350.00, '2025-05-07 12:05:06'),
+(112, 36, 65, 4, 5, 1, 'Certificate of Silver 🥈', '🥈', 'Great job! You\'re making a positive environmental impact.', 35, 350.00, '2025-05-07 12:05:18'),
+(113, 36, 55, 3, 5, 2, 'Certificate of Bronze 🥉', '🥉', 'Nice effort! Keep building sustainable habits.', 45, 450.00, '2025-05-07 12:05:29'),
+(114, 36, 45, 2, 5, 3, 'Certificate of participation 👏', '🌟', 'You\'re almost there! Just a few more changes will go a long way.', 55, 550.00, '2025-05-07 12:05:39'),
+(115, 36, 40, 2, 4, 4, 'Certificate of participation 👏', '💪', 'You\'re making progress. Small steps matter — keep going!', 60, 600.00, '2025-05-07 12:05:52'),
+(116, 36, 35, 2, 3, 5, 'Certificate of participation 👏', '💪', 'You\'re making progress. Small steps matter — keep going!', 65, 650.00, '2025-05-07 12:06:04'),
+(117, 36, 25, 1, 3, 6, 'Certificate of participation 👏', '🌱', 'Every journey starts somewhere — you\'ve taken that first step!', 75, 750.00, '2025-05-07 12:06:13'),
+(120, 47, 0, 0, 0, 0, 'Initial Registration 🎟️', '🎟️', 'Thank you for joining GreenScore!', 0, 99.00, '2025-05-07 14:55:47');
 
 -- --------------------------------------------------------
 
@@ -157,12 +180,14 @@ CREATE TABLE `new_users` (
 --
 
 INSERT INTO `new_users` (`id`, `username`, `email`, `password`, `role`, `created_at`, `cardid`, `status`, `company_name`, `contact_person`, `phone_number`) VALUES
-(34, 'test1', 'test@test.test', '$2y$10$azg83r8x.QMs767x0yLGs.p9cDYfg7FOebTXxGb8H4Avvhk2QH08K', 'admin', '2025-05-01 17:23:54', NULL, 'inactive', 'test1 company', 'testone', '11111111111111111111'),
-(35, 'fumanchu', 'fu@fu', '$2y$10$n4vh11tJxOgY5MPkKfH5Q.0XKnyot30AWMCy.TAAKWC3KYn7MjKgy', 'user', '2025-05-01 17:34:52', NULL, 'deactivated', 'fum fum fum co', 'smoke', '00000000000000000'),
+(34, 'test1', 'test@test.test', '$2y$10$azg83r8x.QMs767x0yLGs.p9cDYfg7FOebTXxGb8H4Avvhk2QH08K', 'admin', '2025-05-01 17:23:54', NULL, 'active', 'test1 company', 'testone', '1234567890'),
+(35, 'fumancher', 'fu@fu', '$2y$10$n4vh11tJxOgY5MPkKfH5Q.0XKnyot30AWMCy.TAAKWC3KYn7MjKgy', 'user', '2025-05-01 17:34:52', NULL, 'active', 'fum fum fum co', 'smoke', '00000000000000000'),
 (36, 'admin@admin', 'admin@admin', '$2y$10$VdFdg3VD8VoaIuHaSG327u3.y8Qy8PHuZ.orewXdhofHYxvgJP09q', 'admin', '2025-05-01 21:47:23', NULL, 'active', 'admin corporation', 'administrator', '00000000000000000000'),
-(37, 'jamoncin', 'pernil@pernil', '$2y$10$Hwwe8aOkZ0lGX1bpCtjP7uOwoEAnIdCStqnJ4U0KuoMzkignZKG2i', 'user', '2025-05-02 00:26:03', NULL, 'active', 'JAM company', 'pig', '64521765476547654312'),
+(37, 'jamoncin', 'pernil@pernil', '$2y$10$Hwwe8aOkZ0lGX1bpCtjP7uOwoEAnIdCStqnJ4U0KuoMzkignZKG2i', 'admin', '2025-05-02 00:26:03', NULL, 'inactive', 'JAM company', 'pig', '64521765476547654312'),
 (39, 'Joe', 'joe@joe.com', '$2y$10$gDVJKK.y0c99dkaMLJrdZOObGQsRo5AN7YlsPglg1vZvJU4IooQ6S', 'user', '2025-05-02 12:24:05', NULL, 'active', 'joe.Corporation', 'Joe DOE', '5555555555555555555'),
-(40, 'cris', 'crif@gmail.com', '$2y$10$7nHeemAINX68isS/zeOY4OYXpof6s48TZ6RAQgfuPOyaWvnwc/o4O', 'user', '2025-05-02 17:20:19', NULL, 'active', 'Crif', 'Crif', '08897956689');
+(40, 'cris', 'crif@gmail.com', '$2y$10$7nHeemAINX68isS/zeOY4OYXpof6s48TZ6RAQgfuPOyaWvnwc/o4O', 'user', '2025-05-02 17:20:19', NULL, 'active', 'Crif', 'Crif', '08897956689'),
+(41, 'Juan', '1@1', '$2y$10$sZKDAfwxbh4GUbD8Z18Spe3DmyNcziIz6cfIb2gR3taMoRri68hau', 'user', '2025-05-02 19:09:47', NULL, 'deactivated', 'One', 'Juan', '11111111111111'),
+(47, 'test2', 'test2@test2', '$2y$10$NQO3zCquzyQNbtt4bEGIsO.FvAF685X.2qwLUxgVMmZs.nGTp.u2O', 'user', '2025-05-07 14:55:47', NULL, 'deactivated', 'test2', 'test2', 'test2');
 
 -- --------------------------------------------------------
 
@@ -228,7 +253,7 @@ ALTER TABLE `success_stories`
 -- AUTO_INCREMENT for table `community_tips`
 --
 ALTER TABLE `community_tips`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT for table `credit_cards`
@@ -240,19 +265,19 @@ ALTER TABLE `credit_cards`
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `green_calculator_results`
 --
 ALTER TABLE `green_calculator_results`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=121;
 
 --
 -- AUTO_INCREMENT for table `new_users`
 --
 ALTER TABLE `new_users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT for table `success_stories`
