73f9446efeeb652af6dcf563ad2b679bbcb3edcc
Lancelcode <127974834+Lancelcode@users.noreply.github.com>
2026-04-24 00:38:01 +0100

    refactor: remove unused DB connections from static info pages

--- pages/info/partner.php
+++ pages/info/partner.php
@@ -1,6 +1,5 @@
 <?php
 require_once __DIR__ . '/../../includes/init.php';
-require_once ROOT_PATH . '/includes/connect_db.php';
 include ROOT_PATH . '/includes/nav.php';
 $b = BASE_URL;
 ?>
@@ -83,4 +82,3 @@ $b = BASE_URL;
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
-<?php mysqli_close($link); ?>
